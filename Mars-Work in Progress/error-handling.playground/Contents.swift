925c2f32587b1f251263e6981cb216f23a50e5ee
Mars Geldard <github@mail.themartianlife.com>
2019-06-04 20:24:22 +1000

    Mars Error Handling

--- Mars-Work in Progress/error-handling.playground/Contents.swift
+++ Mars-Work in Progress/error-handling.playground/Contents.swift
@@ -1,5 +1,7 @@
+import Foundation
+
 // --------------------------------------------------------------
-// ⬇ This code is Pattern-matching:
+// ⬇ This code is setup for the rest:
 // --------------------------------------------------------------
 protocol CelestialBody: CustomDebugStringConvertible {
     var name: String { get }
@@ -11,13 +13,22 @@ extension CelestialBody {
     }
 }
 
-import Foundation
+protocol Spherical {
+    var diameter: Double { get }
+}
+
+extension Spherical {
+    var volume: Double {
+        let radius = self.diameter / 2
+        return (4 / 3) * (Double.pi * pow(radius, 3))
+    }
+}
 
 enum PlanetType {
     case terran, jovian
 }
 
-struct Planet: CelestialBody {
+struct Planet: CelestialBody, Spherical {
     let name: String
     let diameter: Double
     let moonCount: Int
@@ -25,9 +36,14 @@ struct Planet: CelestialBody {
     let habitable: Bool
 }
 
-struct Sun: CelestialBody {
+struct Sun: CelestialBody, Spherical {
     let name: String
     let diameter: Double
+}
+
+struct FieldOfAsteroids: CelestialBody {
+    let name: String
+    let mass: Double
 }
 
 let mercury = Planet(name: "Mercury", diameter: 4879, moonCount: 0, type: .terran, habitable: false)
@@ -39,40 +55,54 @@ let saturn  = Planet(name: "Saturn", diameter: 120536, moonCount: 62, type: .jov
 let uranus  = Planet(name: "Uranus", diameter: 51118, moonCount: 27, type: .jovian, habitable: false)
 let neptune = Planet(name: "Neptune", diameter: 49528, moonCount: 14, type: .jovian, habitable: false)
 
-let planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
-let theSun = Sun(name: "Sol", diameter: 1391000)
+let innerPlanets = [mercury, venus, earth, mars]
+let outerPlanets = [jupiter, saturn, uranus, neptune]
+let planets = innerPlanets + outerPlanets
 
-func volumeOfSphere(with radius: Double) -> Double {
-    return (4 / 3) * (Double.pi * pow(radius, 3))
+let theSun = Sun(name: "Sol", diameter: 1391000)
+let asteroidBelt = FieldOfAsteroids(name: "The Main Asteroid Belt", mass: 3e21)
+let kuiperBelt = FieldOfAsteroids(name: "The Kuiper Belt", mass: 2e23)
+
+let celestialBodies: [CelestialBody] = [theSun] + innerPlanets + [asteroidBelt] + outerPlanets + [kuiperBelt]
+
+// --------------------------------------------------------------
+// ⬇ This code is Pattern-matching:
+// --------------------------------------------------------------
+// pattern matching is useful for categorising and generalising things
+// so many sub-types can be treated as being in different buckets
+// while maintaining unique behaviours
+
+/// if case let, guard let, for case let <===TODO===
+
+/// wildcards <===TODO===
+
+/// where (condition chaining) <===TODO===
+for body in celestialBodies where body is Spherical  {
+    
 }
 
-// if guard for
-
-// wildcards
-
-// where (condition chaining)
-
-// ~= Expression Pattern
+// ~= Expression Pattern is like "contains/isin" in the case of numbers
+// but means "matches pattern"
+print("\n====================\n MOONS\n====================")
 for planet in planets {
     if planet.moonCount == 0 {
-        
-    }
-    
-    if 1..<6 ~= planet.moonCount {
-        
-    }
-    
-    if 6..<20 ~= planet.moonCount {
-        
-    }
-    
-    if 20... ~= planet.moonCount {
-        
+        print("\(planet.name) has no moons.")
+    } else if 1..<6 ~= planet.moonCount {
+        print("\(planet.name) has a small amount of moons!")
+    } else if 6..<20 ~= planet.moonCount {
+        print("\(planet.name) has a considerable amount of moons!")
+    } else if 20... ~= planet.moonCount {
+        print("\(planet.name) has SO many moons!")
+    } else {
+        print("ERROR: There was no information about moon count for \(planet.name)!")
     }
 }
 
+// the operator is inferred in switch cases with range-matching
+// (this code is equivalent to the above)
+print("\n====================\n MOONS\n====================")
 for planet in planets {
-    switch (planet.moonCount) {
+    switch planet.moonCount {
         case 0: print("\(planet.name) has no moons.")
         case 1..<6: print("\(planet.name) has a small amount of moons!")
         case 6..<20: print("\(planet.name) has a considerable amount of moons!")
@@ -81,18 +111,88 @@ for planet in planets {
     }
 }
 
-// overloading
+enum MassCategory{
+    case small, medium, large
+    
+    var volumeRange: Range<Double> {
+        switch self {
+            case .small: return 0..<10e11
+            case .medium: return 10e11..<10e13
+            case.large: return 10e13..<10e50
+        }
+    }
+}
 
+// which means we can overload it to do great things in switch statements
+func ~= (pattern: MassCategory, value: Spherical) -> Bool {
+    return pattern.volumeRange.contains(value.volume)
+}
 
-//: Error handling with optional chaining
+print("\n====================\n MASSES\n====================")
+for body in celestialBodies {
+    guard let sphericalBody  = body as? Spherical else {
+        print("\(body.name) is of too irregular mass to calculate volume!")
+        continue
+    }
+    
+    switch (sphericalBody) {
+        case .small: print("\(body.name) is small in volume!")
+        case .medium: print("\(body.name) is medium in volume!")
+        case .large: print("\(body.name) is large in volume!")
+        default: print("\(body.name) is totally unprecedented in volume!")
+    }
+}
 
-//: .map() and compactMap()
+// --------------------------------------------------------------
+// ⬇ This code is Error handling with optional chaining:
+// --------------------------------------------------------------
 
-//: Throwing and handling errors
-// multiple and rethrows
+// --------------------------------------------------------------
+// ⬇ This code is .map() and compactMap():
+// --------------------------------------------------------------
 
-//: Encoding and decoding
-typealias Codable = Decodable & Encodable
+// --------------------------------------------------------------
+// ⬇ This code is Throwing and handling errors:
+// --------------------------------------------------------------
+
+// --------------------------------------------------------------
+// ⬇ This code is Encoding and decoding:
+// --------------------------------------------------------------
+protocol Spacecraft: Codable {
+    var name: String { get }
+    var designation: String { get }
+}
+
+struct SpaceShuttle: Spacecraft {
+    let name: String
+    let designation: String
+}
+
+struct Rocket: Spacecraft {
+    let name: String
+    let designation: String
+}
+
+struct Fleet: Codable {
+    var shuttles: [SpaceShuttle]
+    var rockets: [Rocket]
+}
+
+let colombia = SpaceShuttle(name: "Colombia", designation: "OV-102")
+let challenger = SpaceShuttle(name: "Challenger", designation: "OV-099")
+let discovery = SpaceShuttle(name: "Discovery", designation: "OV-103")
+let atlantis = SpaceShuttle(name: "Atlantis", designation: "OV-104")
+let endeavour = SpaceShuttle(name: "Endeavour", designation: "OV-105")
+let enterprise = SpaceShuttle(name: "Enterprise", designation: "OV-101")
+
+let shuttles = [colombia, challenger, discovery, atlantis, endeavour]
+let nasaFleet = Fleet(shuttles: [], rockets: [])
+
+let encoder = JSONEncoder()
+let encoded = try encoder.encode(nasaFleet)
+
+///==============================================================
+/// PICK SOME ACTIVITIES
 
 // --------------------------------------------------------------
 // ⬇ This code is Be The Playground:
--- Mars-Work in Progress/generics.playground/Contents.swift
+++ Mars-Work in Progress/generics.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
 
-var str = "Hello, playground"
+// Basic generics (using types instead of values and the benefits that brings)
+
+// The Swift type checker can reason about your code
+
+// Values defined by other values
+
+// Clumsy enum with two cases example
+
+// Modelling something more complex
+
+// Varying the types themselves to do something
+
+// Generic types
+
+// Type parameters
+
+// Type constraints
+
+// Using Array with its proper <> syntax (instead of [])
+
+// Dictionary in Swift is a good example of using generics with protocols to model complex datastructures
+
+// Optionals are also a generic
+
+// Generic function parameters
+
